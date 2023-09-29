24ed012e4dc829f06cd9195efef6185841800424
JohnDoe1001 <102891530+JohnDoe1001@users.noreply.github.com>
2023-09-29 15:53:07 +0700

    Update bash-ai 0.0.0.1.sh

--- bash-ai 0.0.0.1.sh
+++ bash-ai 0.0.0.1.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
+prompt () {
 clear
 echo Welcome to Bash-AI v0.0.0.1-alpha!
-echo Please choose your RAM disk size (in megabytes)
-echo (recommended size: 256MB)
-read size
-echo Please choose your mount point.
-read mount
+read -r "Please choose your RAM disk size \
+(in megabytes) (recommended size: 256MB)" size
+read -r "Please choose your mount point." mount
 [[ -z "$size" ]] && $size=256
+([[ "$size" =~ ^[0-9] ]] && "$size" -eq "$size") || prompt
 [[ -z "$mount" ]] && $mount="./aidata/qindex"
+([[ -d "$mount" ]] && "$mount" -eq "$mount") || prompt
 cd /
 sudo mkdir 
-mount -t tmpfs -o $size 
+mount -t tmpfs -o $size $mount
+}
